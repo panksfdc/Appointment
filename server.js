f5da0fab44bf0f545935f99793d86751a2b2cc88
Sukhpartap Singh <sukhpartap.singh@appliedsystems.com>
2020-07-19 20:04:51 -0400

    Added Body parser

--- server.js
+++ server.js
@@ -6,16 +6,12 @@
 
 let debug = require('debug')('comp307-lesson3:server');
 let http = require('http');
+let bodyParser = require('body-parser')
+
 let express = require("express");
 let app = express();
-
-app.listen(4000, () => {
- console.log("Server running on port 3000");
-});
-
-app.get("/url",(req,res,next) => {
-    res.json(["Tony","Lisa","Michael","Ginger","Food"])
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 /**
  * Get port from environment and store in Express.
@@ -97,3 +93,55 @@ function onListening() {
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
 }
+
+
+
+app.listen(4000, () => {
+  console.log("Server running on port 3000");
+});
+
+app.post("/url", (req, res, next) => {
+
+  const { google } = require('googleapis');
+  const { OAuth2 } = google.auth;
+
+  console.log(req.body.refreshToken);
+  const oAuth2Client = new OAuth2(req.body.clientKey, req.body.apiKey)
+  oAuth2Client.setCredentials({ refresh_token: req.body.refreshToken });
+
+  const calendar = google.calendar({ version: 'v3', auth: oAuth2Client });
+
+
+  let startDate = req.body.startDate;
+  let startTime = req.body.startTime;
+  let googleStartTime = (startDate + 'T' + startTime).split('.000Z')[0] + '-04:00';
+  let googleEndTime = (req.body.startDate + 'T' + req.body.endTime).split('.000Z')[0] + '-04:00';
+  console.log(googleStartTime)
+  const event = {
+    summary: req.body.summary,
+    description: req.body.description,
+    start: {
+      dateTime: googleStartTime,
+      timeZone: 'America/Toronto',
+    },
+    end: {
+      dateTime: googleEndTime,
+      timeZone: 'America/Toronto'
+    },
+    colorId: 1,
+    attendees: [
+      { 'email': req.body.attendee }
+    ]
+  }
+
+  calendar.events.insert({ calendarId: 'primary', resource: event }, err => {
+    if (err) {
+      res.json({ "Message": "Calender Invitation Not Sent" });
+    }
+    else {
+      res.json({ "Message": "Calender Invitation Sent" });
+    }
+  });
+
+ 
+})
